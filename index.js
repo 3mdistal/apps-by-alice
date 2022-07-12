51f051763bccb9a693b6974921ed32ae0851fafc
Alice Alexandra Moore <tempoimmaterial@gmail.com>
2022-07-12 11:39:41 +0200

    CSS transition bugfix.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict";
-alert('Welcome to the site that *will* contain all things Alice Alexandra Moore. For now, what you see is a hulking shell as I get some Javascript to behave correctly. Thanks for your patience. ~Alice');
+// alert('Welcome to the site that *will* contain all things Alice Alexandra Moore. For now, what you see is a hulking shell as I get some Javascript to behave correctly. Thanks for your patience. ~Alice');
 
 let homepageSections = document.querySelectorAll(".homepage-section");
 
