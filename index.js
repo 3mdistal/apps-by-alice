808d8322b502aefccf6715b9cd090328b45ee44f
Alice Alexandra Moore <tempoimmaterial@gmail.com>
2022-07-17 15:32:19 +0200

    Performance fixes. Content appears on click.

--- index.js
+++ index.js
@@ -8,21 +8,73 @@ function showPage() {
     document.body.classList.remove('js-loading');
 }
 
-let homepageSections = document.querySelectorAll(".homepage-section");
+const homepageSections = document.querySelectorAll(".homepage-section");
+const aboutSection = homepageSections[0];
+const studioSection = homepageSections[1];
+const commissionsSection = homepageSections[1];
+const shopSection = homepageSections[1];
+const newsSection = homepageSections[1];
+
+const homepageSectionContent = document.querySelectorAll(".homepage-section-content");
+const aboutContent = homepageSectionContent[0];
+const studioContent = homepageSectionContent[1];
+const commissionsContent = homepageSectionContent[1];
+const shopContent = homepageSectionContent[1];
+const newsContent = homepageSectionContent[1];
 
 for (let elem of homepageSections) {
-    elem.onclick = toggleDisplay;
+    elem.addEventListener("click", (event) => {
+        hide(homepageSections);
+        removeHoverColor(homepageSections);
+        event.target.classList.remove("hide");
+        setTimeout(unsetScaleTransition, 500);
+
+        switch (event.target) {
+            
+        case aboutSection:
+            aboutContent.classList.remove("hide");
+            break;
+        
+        case studioSection:
+            studioContent.classList.remove("hide");
+            break;
+            
+        case commissionsSection:
+            commissionsContent.classList.remove("hide");
+            break;
+            
+        case shopSection:
+            shopContent.classList.remove("hide");
+            break;
+            
+        case newsSection:
+            newsContent.classList.remove("hide");
+            break;
+        
+        }
+    })
 }
 
-function toggleDisplay() {
-    setHide();
-    this.classList.remove("hide");
-    setTimeout(unsetScaleTransition, 500);
-}
-
-function setHide() {
-    for (let elem of homepageSections) {
+function hide(collection) {
+    for (let elem of collection) {
         elem.classList.add("hide");
+    }
+}
+
+function unhide(collection) {
+    for (let elem of collection) {
+        elem.classList.remove("hide");
+    }
+}
+
+function addHoverColor(collection) {
+    for (let elem of collection) {
+        elem.classList.add("hover-color");
+    }
+}
+
+function removeHoverColor(collection) {
+    for (let elem of collection) {
         elem.classList.remove("hover-color");
     }
 }
@@ -39,12 +91,11 @@ function unsetScaleTransition() {
     }
 }
 
-function unsetHide(collection) {
-    for (let elem of collection) {
-        elem.classList.remove("hide");
-        elem.classList.add("hover-color");
-    }
-}
+// function setAnimationOn() {
+//     document.querySelector(".logo").style.animationPlayState = play;
+//     document.querySelector(".site-title").style.animationPlayState = play;
+//     document.querySelector(".subtitle").style.animationPlayState = play;
+// }
 
 
 let lastScrollPosition = 0;
@@ -52,9 +103,12 @@ let lastScrollPosition = 0;
 function scrollTopDetect() {
     if (scrollY < lastScrollPosition
         && scrollY < 200
-        && scrollY > 10) {
+        && scrollY > 5) {
         setTimeout(setScaleTransition, 500);
-        unsetHide(homepageSections);
+        unhide(homepageSections);
+        addHoverColor(homepageSections);
+        hide(homepageSectionContent);
+        // setAnimationOn;
         lastScrollPosition = 0;
     }
 
@@ -62,12 +116,10 @@ function scrollTopDetect() {
 }
 
 
-function listenUserScroll() {
+(function listenUserScroll() {
     window.addEventListener(
         "scroll",
         scrollTopDetect,
         { passive: true }
     )
-}
-
-listenUserScroll();
+})();
