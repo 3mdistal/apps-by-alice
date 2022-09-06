2b8dfcf78151b6cdd175dc63f4df95fa840f712b
Alice Alexandra Moore <emdistal@gmail.com>
2022-09-06 09:11:01 +0000

    Fixed button.

--- src/lib/icons/button.svelte
+++ src/lib/icons/button.svelte
@@ -5,16 +5,17 @@
     export let text
 </script>
 
-<button href={url} style="background-color: {$accentColors[$state]}; color: {$backgroundColors[$state]}">
+<a href={url} target="_blank" style="background-color: {$accentColors[$state]}; color: {$backgroundColors[$state]}">
     {text}
-</button>
+</a>
 
 <style>
-  button {
+  a {
     border-radius: 10px;
     border: none;
     padding: 1em;
     cursor: pointer;
     font-size: clamp(.8rem, 1.2vw, 1.5rem);
+    text-decoration: none;
   }
 </style>
