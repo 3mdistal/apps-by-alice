d0b83bd810898ba462e3f04590259ce6a70f3e1a
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2022-10-09 08:27:49 +0100

    Killed redundancies.

--- src 2/lib/subpages/studio.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import { accentColors, backgroundColors, state } from "../stores";
-</script>
-
-<div class="spacer" />
-
-<p class="header-secondary" style="color:{$accentColors[$state]}">
-    latest edits.
-</p>
