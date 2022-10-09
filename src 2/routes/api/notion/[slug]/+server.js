c33fe68081c418c363afcfcae78e6f01ec504371
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2022-10-09 08:27:49 +0100

    Killed redundancies.

--- src 2/routes/api/notion/[slug]/+server.js
+++
@@ -1,9 +0,0 @@
-import { findName } from "../+server";
-import { json } from "@sveltejs/kit"
-
-export async function GET( { params } ) {
-    const res = await findName(params.slug)
-    return json(
-        { "text": res }
-    )
-}
