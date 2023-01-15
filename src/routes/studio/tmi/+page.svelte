dac4124f89b23014502bfdba36393dbbe7d58e93
Alice Alexandra Moore <emdistal@gmail.com>
2023-01-15 17:02:49 +0000

    Betson & TMI coming soon pages updated.

--- src/routes/studio/tmi/+page.svelte
+++ src/routes/studio/tmi/+page.svelte
@@ -1,1 +1,50 @@
-<p class="text-8xl">The future home of These Makeshift Idiotika.</p>
+<div
+    class="h-[100%] bg-black p-4 tracking-wide text-white children:text-inherit"
+>
+    <h1 class="mb-2 text-5xl md:text-7xl">these makeshift idiotika</h1>
+    <p class="mb-[1em] text-2xl md:text-4xl max-w-[30ch]">
+        a sprawling world of lovers & fiends torn by an ever-tilting
+        landscape
+    </p>
+    <hr class="my-10 mx-10 opacity-50" />
+    <p>
+        <em class="mr-4 text-inherit">tl;dr</em> You'll meet some characters very
+        soon.
+    </p>
+    <hr class="my-10 mx-10 opacity-50" />
+    <div class="max-w-[60ch] children:mb-[1em] children:text-inherit">
+        <p>
+            Disparate connections drive us through life. We're always picking up
+            on fragments of history, never getting the full story. In the
+            fantasy world of TMI, a huge cast of characters in a multitude of
+            places explore their lives through an impossible lens of the past:
+            one where the world itself has shifted, scrambling itself beyond
+            recognition.
+        </p>
+        <p>
+            These Makeshift Idiotika is a long work-in-progress. I've been
+            hacking away at lore, character arcs, and the overall story flow for
+            over eight years. It's gone through various mediums, from graphic
+            novel to short stories to what now is a video game I'm developing in
+            my free time.
+        </p>
+        <p>
+            It's daunting, to be honest, to keep track of it all on my own. This
+            story has gone through many periods of advisement from others, and I
+            see the game will, too. Right now, I'm working on improving my 3D
+            art skills and my programming in Godot game engine. It's slow, but
+            rewarding, to tell the story the way I want.
+        </p>
+        <p>
+            Here on the site I'll be keeping you updated on my progress, but
+            more than that, I'll be starting to show you the intricate world of
+            the idiotika. You'll be meeting characters as I get art finished,
+            you'll hear music as I compose themes, and you'll tour new places as
+            they get shifted around the world.
+        </p>
+        <p>
+            So, here's to sharing the complicated things that go on in our
+            minds. To giving voice to what feels impossible to express.
+        </p>
+    </div>
+</div>
