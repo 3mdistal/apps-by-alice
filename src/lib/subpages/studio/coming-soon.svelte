8854ba007b3b173e5b307956c82408a293a3f2c9
Alice Moore <emdistal@gmail.com>
2023-02-19 13:22:57 +0000

    Deleted weird coming-soon transition.

--- src/lib/subpages/studio/coming-soon.svelte
+++ src/lib/subpages/studio/coming-soon.svelte
@@ -1,14 +1,10 @@
 <script>
     import gsap from "gsap";
     let container;
-    function fade() {
-        gsap.from(container.children, { scale: 0.5, duration: 1, ease: "power1.out" })
-    }
 </script>
 
 <div
     class="h-[100%] bg-black p-4 tracking-wide text-white children:text-inherit"
-    in:fade
     bind:this={container}
 >
     <h1 class="mb-2 text-5xl md:text-7xl">
