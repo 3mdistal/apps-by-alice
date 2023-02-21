41bd1f5795340ef23a7936a6b0b6a609c6f7c3b8
Alice Moore <emdistal@gmail.com>
2023-02-21 23:46:14 +0000

    OG images added.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,49 @@
 <script>
-    import "../app.css"
-    import Logo from '../cms/images/logo/logo.png'
+	import "../app.css";
+	import Logo from "../cms/images/logo/logo.png";
 	import Svgs from "../lib/icons/svgs.svelte";
 </script>
 
 <svelte:head>
-    <meta charset="utf-8" />
-	<link rel="icon" type="image" href={Logo} />
+	<meta charset="utf-8" />
+	<link rel="icon" type="image" href="{Logo}" />
 	<meta name="viewport" content="width=device-width" />
-	<link rel="preconnect" href="https://fonts.googleapis.com">
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 	<link
 		href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
-		rel="stylesheet">
+		rel="stylesheet"
+	/>
+
+	<!-- HTML Meta Tags -->
+	<title>Tempo Immaterial</title>
+	<meta
+		name="description"
+		content="The studio of Alice Alexandra Moore. Creative work, ramblings, commissions and more."
+	/>
+
+	<!-- Facebook Meta Tags -->
+	<meta property="og:url" content="https://www.alicealexandra.com" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Tempo Immaterial" />
+	<meta
+		property="og:description"
+		content="The studio of Alice Alexandra Moore. Creative work, ramblings, commissions and more."
+	/>
+	<meta property="og:image" content="{Logo}" />
+
+	<!-- Twitter Meta Tags -->
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta property="twitter:domain" content="alicealexandra.com" />
+	<meta property="twitter:url" content="https://www.alicealexandra.com" />
+	<meta name="twitter:title" content="Tempo Immaterial" />
+	<meta
+		name="twitter:description"
+		content="The studio of Alice Alexandra Moore. Creative work, ramblings, commissions and more."
+	/>
+	<meta name="twitter:image" content="{Logo}" />
+
+	<!-- Meta Tags Generated via https://www.opengraph.xyz -->
 </svelte:head>
 
 <Svgs />
