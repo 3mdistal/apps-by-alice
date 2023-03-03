a3e30cdf9b5fa6b6ceea31b585faa0ffa3f1e9dc
Alice Moore <emdistal@gmail.com>
2023-03-03 11:38:03 +0000

    Notion preview available.

--- src/lib/notion/notion.ts
+++ src/lib/notion/notion.ts
@@ -1,10 +1,5 @@
-import { Client } from "@notionhq/client";
-import {
-  NOTION_KEY,
-  COMMISSIONS_DB,
-  BLOGS_DB,
-  USER_ID_ALICE,
-} from "$env/static/private";
+import { Client } from '@notionhq/client';
+import { NOTION_KEY, COMMISSIONS_DB, BLOGS_DB, USER_ID_ALICE } from '$env/static/private';
 
 const notion = new Client({ auth: NOTION_KEY });
 
@@ -13,143 +8,133 @@ const blogsDatabaseID = BLOGS_DB;
 
 const today = new Date(Date.now()).toISOString();
 
-export async function addCommission(
-  name: string,
-  email: string,
-  description: string
-) {
-  if (!NOTION_KEY || !COMMISSIONS_DB) {
-    throw Error("Missing API keys from Notion.");
-  }
-  try {
-    const response = await notion.pages.create({
-      parent: { database_id: commissionsDatabaseID },
-      properties: {
-        title: {
-          title: [
-            {
-              text: {
-                content: name,
-              },
-            },
-          ],
-        },
-        Email: {
-          email: email,
-        },
-        Description: {
-          rich_text: [
-            {
-              text: {
-                content: description,
-              },
-            },
-          ],
-        },
-        Notify: {
-          people: [
-            {
-              id: USER_ID_ALICE,
-            },
-          ],
-        },
-      },
-    });
-    return response;
-  } catch (error) {
-    let errorMessage = "Posting to commissions failed generically.";
-    if (error instanceof Error) {
-      errorMessage = error.message;
-    }
-    return errorMessage;
-  }
+export async function addCommission(name: string, email: string, description: string) {
+	if (!NOTION_KEY || !COMMISSIONS_DB) {
+		throw Error('Missing API keys from Notion.');
+	}
+	try {
+		const response = await notion.pages.create({
+			parent: { database_id: commissionsDatabaseID },
+			properties: {
+				title: {
+					title: [
+						{
+							text: {
+								content: name
+							}
+						}
+					]
+				},
+				Email: {
+					email: email
+				},
+				Description: {
+					rich_text: [
+						{
+							text: {
+								content: description
+							}
+						}
+					]
+				},
+				Notify: {
+					people: [
+						{
+							id: USER_ID_ALICE
+						}
+					]
+				}
+			}
+		});
+		return response;
+	} catch (error) {
+		let errorMessage = 'Posting to commissions failed generically.';
+		if (error instanceof Error) {
+			errorMessage = error.message;
+		}
+		return errorMessage;
+	}
 }
 
 export async function getBlogs(slug?: string) {
-  try {
-    const response = await notion.databases.query({
-      database_id: blogsDatabaseID,
-      filter: {
-        and: [
-          {
-            property: "Publication Date",
-            date: {
-              on_or_before: today,
-            },
-          },
-          {
-            property: "Slug",
-            url: {
-              equals: slug ? slug : "",
-            },
-          },
-        ],
-      },
-      sorts: [
-        {
-          direction: "descending",
-          property: "Publication Date",
-        },
-      ],
-    });
-    return response;
-  } catch (error) {
-    let errorMessage = "Posting to commissions failed generically.";
-    if (error instanceof Error) {
-      errorMessage = error.message;
-    }
-    return errorMessage;
-  }
+	try {
+		const response = await notion.databases.query({
+			database_id: blogsDatabaseID,
+			filter: {
+				and: [
+					{
+						property: 'Publication Date',
+						date: {
+							on_or_before: today
+						}
+					},
+					{
+						property: 'Slug',
+						url: {
+							equals: slug ? slug : ''
+						}
+					}
+				]
+			},
+			sorts: [
+				{
+					direction: 'descending',
+					property: 'Publication Date'
+				}
+			]
+		});
+		return response;
+	} catch (error) {
+		let errorMessage = 'Posting to commissions failed generically.';
+		if (error instanceof Error) {
+			errorMessage = error.message;
+		}
+		return errorMessage;
+	}
 }
 
 export async function getContent(slug?: string) {
-  try {
-    let response = [];
+	try {
+		let response = [];
 
-    const query = await notion.databases.query({
-      database_id: blogsDatabaseID,
-      filter: {
-        and: [
-          {
-            property: "Publication Date",
-            date: {
-              on_or_before: today,
-            },
-          },
-          {
-            property: "Slug",
-            url: {
-              equals: slug ? slug : "",
-            },
-          },
-        ],
-      },
-      sorts: [
-        {
-          direction: "descending",
-          property: "Publication Date",
-        },
-      ],
-    });
+		const query = await notion.databases.query({
+			database_id: blogsDatabaseID,
+			filter: {
+				and: [
+					{
+						property: 'Slug',
+						url: {
+							equals: slug ? slug : ''
+						}
+					}
+				]
+			},
+			sorts: [
+				{
+					direction: 'descending',
+					property: 'Publication Date'
+				}
+			]
+		});
 
-    response.push(query);
+		response.push(query);
 
-    let content;
+		let content;
 
-    if (query.results[0]?.id) {
-      content = await notion.blocks.children.list({
-        block_id: query.results[0].id,
-      });
-    }
+		if (query.results[0]?.id) {
+			content = await notion.blocks.children.list({
+				block_id: query.results[0].id
+			});
+		}
 
-    response.push(content);
+		response.push(content);
 
-    return response;
-  } catch (error) {
-    let errorMessage = "Posting to commissions failed generically.";
-    if (error instanceof Error) {
-      errorMessage = error.message;
-    }
-    return errorMessage;
-  }
+		return response;
+	} catch (error) {
+		let errorMessage = 'Posting to commissions failed generically.';
+		if (error instanceof Error) {
+			errorMessage = error.message;
+		}
+		return errorMessage;
+	}
 }
