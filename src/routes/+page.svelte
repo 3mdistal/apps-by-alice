73b54ba98b5f19d2d78b869cb40529878dfccea8
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2023-03-14 10:53:23 +0000

    Preloading implemented correctly.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,10 @@
 	import SiteTitle from '../lib/homepage/site-title.svelte';
 	import { state } from '../lib/stores';
 	import { onMount } from 'svelte';
+	import Flowers from '$images/about/flowers-in-hand.webp';
 	import Piano from '$images/studio/piano-with-chair.webp';
+	import Rabbit from '$images/commissions/rabbit-in-hat.webp';
+	import Bird from '$images/blog/bird.webp';
 
 	onMount(() => {
 		state.set('home');
@@ -12,7 +15,10 @@
 </script>
 
 <svelte:head>
+	<link rel="preload" as="image" href={Flowers} />
 	<link rel="preload" as="image" href={Piano} />
+	<link rel="preload" as="image" href={Rabbit} />
+	<link rel="preload" as="image" href={Bird} />
 </svelte:head>
 
 <div class="relative h-[100dvh] flex-col overflow-hidden pt-4 md:pt-[6vh]">
