070be89f257f120a29a540936b864a43583df581
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2023-03-26 11:27:16 +0100

    Improved layout.

--- src/routes/(landing-pages)/studio/hfc/+page.svelte
+++ src/routes/(landing-pages)/studio/hfc/+page.svelte
@@ -1,14 +1,16 @@
 <script>
-	import Piano from '$images/studio/piano-hfc.jpg';
+	import Piano from '$images/studio/hfc/hero-hfc-lq.webp';
 </script>
 
 <div class="bg-black">
-	<div class="flex h-[100vh] items-center justify-center">
-		<div class="relative mx-auto aspect-video lg:w-[80%]">
-			<div class="absolute top-[30%] left-[10%] bg-[hsla(30,_0%,_100%,_70%)] p-5 font-light">
-				<h1 class="text-2xl md:text-4xl lg:text-5xl xl:text-6xl">hymns for calliope</h1>
+	<div>
+		<div class="relative mx-auto aspect-video md:aspect-auto md:h-[100vh]">
+			<div class="z-10 absolute top-[30%] left-[10%] ">
+				<h1 class="text-2xl md:text-4xl lg:text-5xl xl:text-6xl text-white">hymns for calliope</h1>
+				<p class="text-white">poems</p>
 			</div>
-			<img class="rounded-3xl" src={Piano} alt="A broken piano in front of a building." />
+			<div class="absolute w-[100%] h-[100%] bg-black opacity-50" />
+			<img class="object-[50%_70%]" src={Piano} alt="A broken piano in front of a building." />
 		</div>
 	</div>
 	<div class="flex flex-col items-center px-5 md:px-10 [&_p]:text-[#fafafa]">
