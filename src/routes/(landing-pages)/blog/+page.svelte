7563ce95b840d048bbf4cd42393ddf5d36f83d91
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2023-04-11 13:36:07 +0100

    Visual update.

--- src/routes/(landing-pages)/blog/+page.svelte
+++ src/routes/(landing-pages)/blog/+page.svelte
@@ -1,13 +1,27 @@
 <script lang="ts">
-	import LandingPage from '$lib/subpages/landing-page.svelte';
 	import Bird from '$images/blog/bird.webp';
 	import Blog from '$lib/subpages/blog.svelte';
 	import type { QueryDatabaseResponse } from '@notionhq/client/build/src/api-endpoints';
+	import { onMount } from 'svelte';
+	import { gsap } from 'gsap';
 
 	export let data: { post: QueryDatabaseResponse };
 
 	const accent = '#d1dce7';
 	const background = '#838391';
+
+	function fadeIn() {
+		gsap.to('.opacity-0', {
+			delay: 0.25,
+			duration: 1,
+			opacity: 1,
+			ease: 'power2.inOut'
+		});
+	}
+
+	onMount(() => {
+		fadeIn();
+	});
 </script>
 
 <svelte:head>
@@ -18,18 +32,18 @@
 	/>
 </svelte:head>
 
-<div style="background-color: {background}">
-	<LandingPage
-		header="unearth some thoughts."
-		description="I'll be using the blog to keep you up to date on creative
-        work, site revisions, and life updates. Check out the posts below!"
-		src={Bird}
-		alt="A painting of a colorful bird in flight."
-		flexReverse
-		{accent}
-		textWhite
-	/>
-	<div class="mx-auto w-[90%] py-20 md:w-[60%]">
+<div
+	class="opacity-0 min-h-[100vh] flex items-center justify-center"
+	style="background-color: {background}"
+>
+	<div class="justify-center flex w-full h-full fixed opacity-10">
+		<img
+			src={Bird}
+			alt="A painting of a colorful bird in flight."
+			class="object-right md:object-[50%_35%]"
+		/>
+	</div>
+	<div class="w-[90%] py-20 md:w-[60%]">
 		<Blog {accent} {data} />
 	</div>
 </div>
