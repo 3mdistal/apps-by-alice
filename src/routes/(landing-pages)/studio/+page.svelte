de27b3bb430fa97403ae5143ad57f2e6e2bfcd6b
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2023-09-13 20:18:49 +0100

    Another FOUC'ing fix.

--- src/routes/(landing-pages)/studio/+page.svelte
+++ src/routes/(landing-pages)/studio/+page.svelte
@@ -11,11 +11,9 @@
 
 	function populate() {
 		const tl = gsap.timeline();
-		tl.fromTo(
-			'img.fixed',
-			{ opacity: 0 },
-			{ duration: 1, opacity: 0.4, ease: 'power2.inOut' }
-		).fromTo('.card-div > *', { opacity: 0 }, { opacity: 1, stagger: 0.1 }, '< .5');
+		tl.to('.card-div', { opacity: 1, duration: 0.5 })
+			.fromTo('img.fixed', { opacity: 0 }, { duration: 1, opacity: 0.4, ease: 'power2.inOut' }, '<')
+			.fromTo('.card-div > *', { opacity: 0 }, { opacity: 1, stagger: 0.1 }, '< .5');
 	}
 
 	onMount(() => {
@@ -74,7 +72,9 @@
 	<div class="h-12" />
 
 	<!-- Cards -->
-	<div class="card-div flex flex-wrap items-start justify-center gap-12 sm:justify-center sm:px-10">
+	<div
+		class="card-div flex flex-wrap items-start justify-center gap-12 opacity-0 sm:justify-center sm:px-10"
+	>
 		{#each cards as { properties: { Title: { title: [{ plain_text: title }] }, Subtitle: subtitle, 'Shortened Logo Text': logo, Image: { url: src }, ImageAlt: { rich_text: [{ text: { content: alt } }] }, Description: description, ButtonText: { rich_text: [{ text: { content: buttonText } }] }, Destination: { url } } }}
 			<StudioCard {title} {subtitle} {logo} {src} {alt} {description} {buttonText} {url} />
 		{/each}
