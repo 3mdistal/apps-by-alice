acebef44458f6556070f81b35dc4e1925938824c
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2023-09-15 14:08:26 +0100

    Improved product.

--- src/routes/(landing-pages)/studio/illustrations/+page.svelte
+++ src/routes/(landing-pages)/studio/illustrations/+page.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	export let data;
 
 	const {
@@ -7,41 +7,49 @@
 
 	let currentURL = '';
 	let all = true;
+	const lowQuality = '?tr=f-webp,w-400,q-50';
+	let scrollPos = 0;
+	let classList = ['sm:columns-2', 'md:columns-3', 'lg:columns-4', 'xl:columns-5'];
+	let artGrid;
 
 	function handleClick(event) {
-		if (all === true) {
-			currentURL = event.target.src;
-			console.log(currentURL);
-			document
-				.getElementsByClassName('art-grid')[0]
-				.classList.remove('sm:columns-2', 'md:columns-3', 'lg:columns-4', 'xl:columns-5');
+		currentURL = event.target.src;
+		scrollPos = window.scrollY;
+		artGrid.classList.remove(...classList, 'p-2');
 
-			all = false;
-			history.pushState(null, null, '/studio/illustrations/');
-			window.addEventListener('popstate', () => {
-				currentURL = '';
-				document
-					.getElementsByClassName('art-grid')[0]
-					.classList.add('sm:columns-2', 'md:columns-3', 'lg:columns-4', 'xl:columns-5');
-				all = true;
-			});
-			return;
-		}
-		if (all === false) {
+		all = false;
+		history.pushState(null, '', '/studio/illustrations/');
+		window.addEventListener('popstate', () => {
 			currentURL = '';
-			document
-				.getElementsByClassName('art-grid')[0]
-				.classList.add('sm:columns-2', 'md:columns-3', 'lg:columns-4', 'xl:columns-5');
+			artGrid.classList.add(...classList);
 			all = true;
-			window.removeEventListener('popstate', () => {
-				currentURL = '';
-				document
-					.getElementsByClassName('art-grid')[0]
-					.classList.add('sm:columns-2', 'md:columns-3', 'lg:columns-4', 'xl:columns-5');
-				all = true;
-			});
-			return;
-		}
+		});
+		return;
+	}
+
+	function handleClickB() {
+		currentURL = '';
+		artGrid.classList.add(...classList, 'p-2');
+		all = true;
+		window.removeEventListener('popstate', () => {
+			currentURL = '';
+			artGrid.classList.add(...classList);
+		});
+		all = true;
+		return;
+	}
+
+	function scroll() {
+		window.scrollTo(0, scrollPos);
+	}
+
+	function lazyLoad() {
+		const img = document.querySelector('img[data-src]');
+		console.log(img);
+		img.src = img.dataset.src;
+		img.onload = () => {
+			img.removeAttribute('data-src');
+		};
 	}
 </script>
 
@@ -55,23 +63,31 @@
 
 <!-- Art Grid -->
 <div
-	class="art-grid h-full w-full gap-x-0 bg-gray-800 p-2 sm:columns-2 md:columns-3 lg:columns-4 xl:columns-5"
+	class="h-full w-full gap-x-0 bg-gray-800 p-2 sm:columns-2 md:columns-3 lg:columns-4 xl:columns-5"
+	bind:this={artGrid}
 >
 	{#each results as painting}
 		{#if all}
 			<img
-				src={painting.properties.Image.url + '?tr=f-webp,q-10'}
+				src={painting.properties.Image.url + lowQuality}
 				alt=""
 				class="w-full p-2 transition-all duration-300 ease-in-out sm:hover:scale-105"
 				on:click={handleClick}
+				on:contextmenu|preventDefault
+				use:scroll
 			/>
-		{:else if currentURL === painting.properties.Image.url + '?tr=f-webp,q-10'}
-			<img
-				src={painting.properties.Image.url + '?tr=f-webp,q-50'}
-				alt=""
-				class="w-full p-2 transition-all duration-300 ease-in-out sm:hover:scale-105"
-				on:click={handleClick}
-			/>
+		{:else if currentURL === painting.properties.Image.url + lowQuality}
+			<div class="h-screen w-screen">
+				<img
+					src={painting.properties.Image.url + lowQuality}
+					data-src={painting.properties.Image.url + '?tr=f-webp,q-50'}
+					alt=""
+					class="h-full w-full object-contain"
+					on:click={handleClickB}
+					on:contextmenu|preventDefault
+					on:load={lazyLoad}
+				/>
+			</div>
 		{/if}
 	{/each}
 </div>
