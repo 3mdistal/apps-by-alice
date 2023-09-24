48f6fd1862829f0b6132fd6ba61e409c7a5a9f89
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2023-09-24 10:21:04 +0100

    Coming soon completed, can merge POC.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -52,12 +52,19 @@
 	<div class="relative min-h-screen w-screen">
 		<div class="absolute h-full w-full bg-[var(--dark)] opacity-70" />
 		<div class="relative flex min-h-screen w-full items-center justify-center">
-			<div class="w-1/2">
-				<h1 class="mb-2 font-serif text-6xl text-[var(--light)]">
+			<div class="w-3/4">
+				<h1 class="mb-2 font-serif text-3xl text-[var(--light)] md:text-6xl">
 					<span class="italic">anthropotpourri</span> (n)
 				</h1>
-				<p class="text-xl text-[var(--midLight)]">
+				<p class="mb-4 text-xl text-[var(--midLight)]">
 					The cintematographic works of Shorouk Elkobrsi.
+				</p>
+				<p class="text-xl text-[var(--midLight)]">
+					The building of this studio is currently in-progress. To see current work, please visit
+					the <a
+						class="font-bold text-[var(--light)] hover:text-[var(--mid)]"
+						href="https://www.instagram.com/anthropotpourri/">Anthropotpourri Instagram</a
+					>.
 				</p>
 			</div>
 		</div>
@@ -65,9 +72,9 @@
 </div>
 
 <div id="studio" class="flex min-h-screen w-screen items-center justify-center bg-[var(--dark)]">
-	<p class="font-serif text-6xl text-[var(--light)]">studio</p>
+	<p class="font-serif text-3xl text-[var(--light)] md:text-6xl">studio coming soon...</p>
 </div>
 
 <div id="about" class="flex min-h-screen w-screen items-center justify-center bg-[var(--midDark)]">
-	<p class="font-serif text-6xl text-[var(--light)]">about</p>
+	<p class="font-serif text-3xl text-[var(--light)] md:text-6xl">about coming soon...</p>
 </div>
--- src/routes/menu.svelte
+++ src/routes/menu.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
 	import { menuOpen } from '$lib/stores';
 
-	function handleMenuOpen(e: KeyboardEvent | MouseEvent) {
-		if ((e instanceof KeyboardEvent && e.key == 'Enter') || e instanceof MouseEvent) {
+	function handleMenuOpen(e: KeyboardEvent | MouseEvent | TouchEvent) {
+		if (
+			(e instanceof KeyboardEvent && e.key == 'Enter') ||
+			e instanceof MouseEvent ||
+			e instanceof TouchEvent
+		) {
 			menuOpen.set(true);
+		}
+		return;
+	}
+
+	function handleMenuClose(e: KeyboardEvent | MouseEvent) {
+		if ((e instanceof KeyboardEvent && e.key == 'Enter') || e instanceof MouseEvent) {
+			menuOpen.set(false);
 		}
 		return;
 	}
@@ -12,22 +23,32 @@
 {#if $menuOpen}
 	<div
 		id="menu-open"
-		class="fixed top-0 z-10 flex h-screen w-screen flex-col items-center justify-center gap-y-[8%] bg-[var(--dark)] [&_a]:cursor-pointer [&_p]:font-serif [&_p]:text-6xl [&_p]:italic [&_p]:text-[var(--light)] hover:[&_a]:[&_p]:text-[var(--mid)]"
+		class="fixed top-0 z-10 flex h-screen w-screen flex-col items-center justify-center gap-y-[8%] bg-[var(--midLight)] [&_a]:cursor-pointer [&_p]:font-serif [&_p]:text-6xl [&_p]:italic [&_p]:text-[var(--midDark)] hover:[&_p]:text-[var(--mid)]"
 	>
-		<a href="./#home" on:click={() => menuOpen.set(false)}>
+		<a href="./#home" on:click={handleMenuClose}>
 			<p>home</p>
 		</a>
-		<a href="./#studio" on:click={() => menuOpen.set(false)}>
+		<a href="./#studio" on:click={handleMenuClose}>
 			<p>studio</p>
 		</a>
-		<a href="/#about" on:click={() => menuOpen.set(false)}>
+		<a href="/#about" on:click={handleMenuClose}>
 			<p>about</p>
 		</a>
+	</div>
+
+	<div
+		class="fixed right-8 top-8 z-10 flex h-14 w-14 items-center justify-center rounded-full border-2 border-[var(--midDark)]"
+		on:keydown={handleMenuClose}
+		on:pointerdown={handleMenuClose}
+		role="button"
+		tabindex="0"
+	>
+		<p class="text-center font-serif text-3xl italic text-[var(--midDark)]">a</p>
 	</div>
 {:else}
 	<div
 		class="fixed right-8 top-8 z-10 flex h-14 w-14 items-center justify-center rounded-full border-2 border-[var(--light)]"
-		on:click={handleMenuOpen}
+		on:pointerdown={handleMenuOpen}
 		on:keydown={handleMenuOpen}
 		role="button"
 		tabindex="0"
