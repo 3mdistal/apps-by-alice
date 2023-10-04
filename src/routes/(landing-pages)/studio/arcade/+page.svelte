78f28542442bda8fc1004bdf26f814cd3fc28e8a
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2023-10-04 11:51:54 +0100

    Arcade page done.

--- src/routes/(landing-pages)/studio/arcade/+page.svelte
+++ src/routes/(landing-pages)/studio/arcade/+page.svelte
@@ -1,34 +1,37 @@
 <div
-	class="m-auto flex min-h-screen w-screen max-w-[50ch] flex-col place-items-center gap-y-10 bg-blue-200 py-10 text-2xl"
+	class="flex min-h-screen w-screen flex-col place-items-center gap-y-10 bg-blue-200 px-10 py-10 text-2xl lg:px-20 xl:px-40"
 >
-	<h1 class="text-4xl">100 Projects 'til I get good.</h1>
+	<h1 class="text-center text-4xl">interactive experimentation</h1>
 
-	<div class="[&_p]:mb-10">
-		<p>So, I'm doing something a bit unhinged.</p>
-		<p>
-			I've been obsessed with making games for years. In that time, I've taught myself so much about
-			game design, music creation, art, story—essentially everything except for coding the actual
-			game.
-		</p>
-		<p>
-			I've tries a few game engines and I've found them tough to use, largely because they require a
-			lot of memorization, and I work best when I understand the whole system.
-		</p>
-		<p>
-			The past few years I've been very into web development, and while I knew in the back of my
-			head that making games inside of JavaScript is possible, it never really clicked with me that
-			this would be the most fun way to do it, since it requires making all your own physics,
-			organization, etc.
-		</p>
-		<p>
-			So, I'm going to be just . . . making shit here, and I'll start sharing this page out once
-			they're any good, but for now it's just a new project every day or so until I understand
-			what's going on. I'll chart my progress here.
-		</p>
+	<div class="flex flex-col-reverse gap-y-10 md:flex-row">
+		<div class="md:w-1/2 [&_p]:mb-10">
+			<p>So, I'm doing something a bit unhinged. (Shocker.)</p>
+			<p>
+				I've been obsessed with making games for years. In that time, I've taught myself so much
+				about game design, music creation, art, story—essentially everything except for coding the
+				actual game.
+			</p>
+			<p>
+				Don't get me wrong. I've tried a few game engines, but I've found them tough to for me,
+				largely because they abstract the underlying systems (physics, organization, etc.) away from
+				me, and I work best when I understand the whole system.
+			</p>
+			<p>
+				The past few years I've been very into web development, and while I knew in the back of my
+				head that making games inside of JavaScript is possible, it never really clicked with me
+				that this might be the most fun way to do it—mastering one environment (the web) instead of
+				splitting my time here and in game engines.
+			</p>
+			<p>
+				So, I'm going to... just be making experimental shit here. And by shit, I mean it's kinda
+				indecipherable to anyone but me who's learning. It just keeps me accountable to post
+				publicly, and maybe just maybe after the first few learnings, there will be something real
+				to share.
+			</p>
+		</div>
+		<div class="flex flex-col md:w-1/2 md:items-center [&_p]:text-blue-500 hover:[&_p]:underline">
+			<a href="arcade/breakout"><p>Project 1: Breakout</p></a>
+			<a href="arcade/constellations"><p>Project 2: Constellations</p></a>
+		</div>
 	</div>
-
-	<a href="arcade/breakout"><p class="text-blue-400 hover:underline">Project 1: Breakout</p></a>
-	<a href="arcade/constellations"
-		><p class="text-blue-400 hover:underline">Project 2: Constellations</p></a
-	>
 </div>
