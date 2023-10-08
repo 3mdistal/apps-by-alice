5db2761bd7429ac4eb4b1293c5e911f6de1373f0
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2023-10-08 19:05:15 +0100

    Proof of concept.

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,78 @@
-<script>
+<script lang="ts">
 	import Slide from './slide.svelte';
+
+	const logos = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+	const entries = [
+		{
+			heading: 'My name is Shorouk',
+			imgSrc: 'https://unsplash.it/1200/1200',
+			imgAlt: 'A picture of me',
+			text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error voluptatum, sapiente laboriosam, minus sint, eos quos a porro optio iure neque in repudiandae distinctio voluptatibus obcaecati molestiae. Quas, voluptatibus accusantium?'
+		},
+		{
+			heading: 'This is my about page',
+			imgSrc: 'https://unsplash.it/1500/1500',
+			imgAlt: 'A picture of me',
+			text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error voluptatum, sapiente laboriosam, minus sint, eos quos a porro optio iure neque in repudiandae distinctio voluptatibus obcaecati molestiae. Quas, voluptatibus accusantium?'
+		},
+		{
+			heading: 'You learned about me',
+			imgSrc: 'https://unsplash.it/1100/1100',
+			imgAlt: 'A picture of me',
+			text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error voluptatum, sapiente laboriosam, minus sint, eos quos a porro optio iure neque in repudiandae distinctio voluptatibus obcaecati molestiae. Quas, voluptatibus accusantium?'
+		},
+		{
+			heading: 'Because I am the best',
+			imgSrc: 'https://unsplash.it/1300/1300',
+			imgAlt: 'A picture of me',
+			text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error voluptatum, sapiente laboriosam, minus sint, eos quos a porro optio iure neque in repudiandae distinctio voluptatibus obcaecati molestiae. Quas, voluptatibus accusantium?'
+		}
+	];
+
+	let currentSlide = 0;
+	let showSlide = 0; // intermediate state
+	let scrollLock = 0;
+
+	const changeSlide = (e: WheelEvent) => {
+		const direction = e.deltaY > 0 ? 1 : -1;
+
+		currentSlide += direction;
+
+		if (currentSlide < 0) currentSlide = 0;
+		if (currentSlide > entries.length - 1) currentSlide = entries.length - 1;
+
+		showSlide = currentSlide;
+
+		setTimeout(() => {
+			scrollLock = currentSlide;
+		}, 2000);
+	};
+
+	function throttle(func: Function, delay: number) {
+		let lastCall = 0;
+		return (...args: any[]) => {
+			const now = new Date().getTime();
+			if (now - lastCall < delay) {
+				return;
+			}
+			lastCall = now;
+			return func(...args);
+		};
+	}
+
+	function handleScroll(e: WheelEvent) {
+		if (Math.abs(e.deltaY) < 10) {
+			e.preventDefault();
+		}
+
+		const direction = e.deltaY > 0 ? 1 : -1;
+		if ((direction > 0 && scrollLock < entries.length - 1) || (direction < 0 && scrollLock > 0)) {
+			e.preventDefault();
+		}
+	}
+
+	const throttledChangeSlide = throttle(changeSlide, 1250);
 </script>
 
 <svelte:head>
@@ -7,10 +80,10 @@
 </svelte:head>
 
 <main
-	class="flex flex-col items-center justify-center bg-[var(--dark)] px-4 text-[var(--midLight)]"
+	class="h-[100svh] snap-y snap-mandatory overflow-y-scroll bg-[var(--dark)] px-4 text-[var(--midLight)] [&>*]:snap-start [&>*]:snap-always"
 >
 	<!-- Intro -->
-	<div class="flex min-h-[100svh] max-w-[90%] flex-col items-center justify-center gap-y-4">
+	<div class="flex h-[100svh] max-w-[90%] flex-col items-center justify-center gap-y-4">
 		<h1 class="text-8xl">Shorouk <br /> Elkobrsi</h1>
 		<!-- todo: Make dynamic ethos text. -->
 		<p class="max-w-[40ch]">
@@ -21,7 +94,31 @@
 	</div>
 
 	<!-- Slideshow -->
-	<Slide src={`https://unsplash.it/1200/1200`} />
-	<Slide src={`https://unsplash.it/1000/1000`} />
-	<Slide src={`https://unsplash.it/1100/1100`} />
+	<div class="h-[100svh] overflow-hidden" on:wheel={throttledChangeSlide} on:wheel={handleScroll}>
+		{#key showSlide}
+			<Slide
+				heading={entries[showSlide].heading}
+				src={entries[showSlide].imgSrc}
+				alt={entries[showSlide].imgAlt}
+				text={entries[showSlide].text}
+			/>
+		{/key}
+	</div>
+
+	<!-- Logo Wall -->
+	<div class="flex h-[100svh] max-w-[90%] flex-col items-center justify-center gap-y-10">
+		<h2>People who gave me their trust:</h2>
+		<!-- todo: put wall here -->
+		<div class="grid grid-cols-3 gap-x-2 gap-y-6 [&_img]:rounded-lg">
+			{#each logos as logo}
+				<img src="https://unsplash.it/500/200" alt={logo} />
+			{/each}
+		</div>
+	</div>
 </main>
+
+<div class="hidden">
+	{#each entries as entry}
+		<img src={entry.imgSrc} alt="" />
+	{/each}
+</div>
--- src/routes/about/slide.svelte
+++ src/routes/about/slide.svelte
@@ -1,17 +1,151 @@
-<script>
+<script lang="ts">
+	import { gsap } from 'gsap';
+
 	export let src = 'https://unsplash.it/1000/1000';
-	export let headline = 'A sample headline';
+	export let alt = 'A picture of me';
+	export let heading = 'A sample headline';
 	export let text = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum magnam quidem est dolorem
 		tempora? Sequi, quasi dolor. Harum animi ab similique, magnam aliquid sint, in quaerat, hic
 		veritatis provident assumenda?`;
+
+	const outDuration = 0.5;
+	const inDuration = 0.25;
+	const blinkHalfDuration = 0.1;
+
+	function textIn(target: HTMLParagraphElement) {
+		const tl = gsap.timeline();
+		tl.fromTo(
+			target,
+			{
+				opacity: 0,
+				y: 100
+			},
+			{
+				opacity: 1,
+				y: 0,
+				duration: inDuration,
+				delay: outDuration
+			}
+		);
+
+		return {
+			duration: (inDuration + outDuration) * 1000
+		};
+	}
+
+	function textOut(target: HTMLElement) {
+		const tl = gsap.timeline();
+		tl.fromTo(
+			target,
+			{
+				opacity: 1,
+				y: 0
+			},
+			{
+				duration: outDuration,
+				opacity: 0,
+				y: 100
+			}
+		);
+
+		return {
+			duration: outDuration * 1000
+		};
+	}
+
+	function curtainTopOut(target: HTMLDivElement) {
+		const tl = gsap.timeline();
+		tl.fromTo(
+			target,
+			{
+				y: '-100%'
+			},
+			{
+				delay: outDuration - blinkHalfDuration,
+				duration: blinkHalfDuration,
+				y: '0%'
+			}
+		);
+
+		return {
+			duration: 100
+		};
+	}
+
+	function curtainBottomOut(target: HTMLDivElement) {
+		const tl = gsap.timeline();
+		tl.fromTo(
+			target,
+			{
+				y: '100%'
+			},
+			{
+				delay: outDuration - blinkHalfDuration,
+				duration: blinkHalfDuration,
+				y: '0%'
+			}
+		);
+
+		return {
+			duration: blinkHalfDuration * 1000
+		};
+	}
+
+	function curtainTopIn(target: HTMLDivElement) {
+		const tl = gsap.timeline();
+		tl.fromTo(
+			target,
+			{
+				y: '0%'
+			},
+			{
+				delay: outDuration,
+				duration: blinkHalfDuration,
+				y: '-100%'
+			}
+		);
+
+		return {
+			duration: blinkHalfDuration * 1000
+		};
+	}
+
+	function curtainBottomIn(target: HTMLDivElement) {
+		const tl = gsap.timeline();
+		tl.fromTo(
+			target,
+			{
+				y: '0%'
+			},
+			{
+				delay: outDuration,
+				duration: blinkHalfDuration,
+				y: '100%'
+			}
+		);
+
+		return {
+			duration: blinkHalfDuration * 1000
+		};
+	}
 </script>
 
 <div class="flex h-[100svh] max-w-[90%] flex-col items-center justify-center">
-	<div class="mb-10 h-[40%]">
-		<img class="h-full w-full rounded-2xl" {src} alt="" />
+	<div class="relative mb-10 h-[40%] overflow-hidden rounded-2xl">
+		<img class="h-full w-full" {src} {alt} />
+		<div
+			class="absolute top-0 h-[50%] w-full translate-y-[-100%] bg-black"
+			out:curtainTopOut
+			in:curtainTopIn
+		></div>
+		<div
+			class="absolute bottom-0 h-[50%] w-full translate-y-[100%] bg-black"
+			out:curtainBottomOut
+			in:curtainBottomIn
+		></div>
 	</div>
-	<h2 class="text-center">{headline}</h2>
-	<p class="max-w-[30ch] text-[1rem]">
+	<h2 class="text-center" in:textIn out:textOut>{heading}</h2>
+	<p class="max-w-[30ch] text-[1rem]" in:textIn out:textOut>
 		{text}
 	</p>
 </div>
