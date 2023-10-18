09494389eef2b9081ea0800a9a1831751c66a059
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2023-10-18 10:15:55 +0100

    Footer fixed on mobile.

--- src/routes/footer.svelte
+++ src/routes/footer.svelte
@@ -9,14 +9,14 @@
 	}
 </script>
 
-<div class="grid min-h-screen w-screen place-items-center bg-[var(--midDark)] px-80 py-40">
+<div class="grid min-h-screen w-screen place-items-center bg-[var(--midDark)] px-4 sm:px-80">
 	<div
-		class="grid h-full w-full place-content-center place-items-center gap-y-40 rounded-2xl bg-[var(--midLight)] pt-20"
+		class="sm:max-h- grid h-full max-h-[60%] w-full place-content-center place-items-center gap-y-28 rounded-2xl bg-[var(--midLight)] pt-10 sm:max-h-[75%] sm:gap-y-40 sm:pt-20"
 	>
-		<div class="grid place-items-center gap-y-10">
+		<div class="grid place-items-center gap-y-4 sm:gap-y-10">
 			<h2 class="text-[var(--midDark)]">Get in touch.</h2>
 			<div
-				class="grid max-w-[250px] grid-cols-3 place-items-center gap-x-4 [&_path]:fill-[var(--midDark)] [&_path]:duration-100 [&_path]:hover:[&_a]:fill-[var(--mid)]"
+				class="grid max-w-[150px] grid-cols-3 place-items-center gap-x-4 sm:max-w-[250px] [&_path]:fill-[var(--midDark)] [&_path]:duration-100 [&_path]:hover:[&_a]:fill-[var(--mid)]"
 			>
 				<a href="https://www.instagram.com/anthropotpourri/">
 					<Instagram />
@@ -29,7 +29,9 @@
 				</a>
 			</div>
 		</div>
-		<p class="text-xl text-[var(--midDark)]">
+
+		<!-- Desktop Version -->
+		<p class="hidden text-center text-xl text-[var(--midDark)] sm:block">
 			© Shorouk Elkobrsi {getCurrentYear()}. All Rights Reserved. <span class="mx-4">|</span> Site
 			by
 			<a
@@ -37,5 +39,19 @@
 				href="https://alicealexandra.com">Alice Alexandra Moore</a
 			>
 		</p>
+
+		<!-- Mobile Version -->
+		<div class="sm:hidden">
+			<p class="mb-2 text-center text-sm text-[var(--midDark)]">
+				© Shorouk Elkobrsi {getCurrentYear()}. All Rights Reserved.
+			</p>
+			<p class="text-center text-sm text-[var(--midDark)]">
+				Site by
+				<a
+					class="relative bottom-[.2em] font-['Allison_Script'] text-2xl text-[var(--midDark)]"
+					href="https://alicealexandra.com">Alice Alexandra Moore</a
+				>
+			</p>
+		</div>
 	</div>
 </div>
