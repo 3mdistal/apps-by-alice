e230e1b1f6eebc0b8f087750b02ef08d86cf50ff
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2024-02-26 06:07:15 -0500

    Speed insights added.

--- apps/tempo-immaterial/src/routes/+layout.svelte
+++ apps/tempo-immaterial/src/routes/+layout.svelte
@@ -6,6 +6,10 @@
 	import { dev } from '$app/environment';
 	import { inject } from '@vercel/analytics';
 	inject({ mode: dev ? 'development' : 'production' });
+
+	// Vercel Speed Insights
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+	injectSpeedInsights();
 </script>
 
 <svelte:head>
