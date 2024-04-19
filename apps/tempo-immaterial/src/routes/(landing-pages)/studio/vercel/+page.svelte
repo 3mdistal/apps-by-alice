700de113ffdc84a272e7d1eb41ad618d4fd6c53d
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2024-04-18 20:48:39 -0400

    Vercel work featured. (Iteration one.)

---
+++ apps/tempo-immaterial/src/routes/(landing-pages)/studio/vercel/+page.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+    import PublicationsSection from "./publications-section.svelte";
+    import type { ProfessionalPublications } from './+page.server'
+
+    export let data: Data;
+
+    type Data = {
+        publicationList: {
+            results: Array<ProfessionalPublications>;
+        };
+    }
+
+    const {
+        publicationList: { results: publications },
+    } = data;
+</script>
+
+<svelte:head>
+    <title>
+        Vercel Work
+    </title>
+    <meta name="description" content=" The work accomplished at Vercel by Alice Alexandra Moore.">
+</svelte:head>
+
+<main>
+    <h1 class="text-8xl">
+        Vercel Work
+    </h1>
+    <PublicationsSection
+        publications={publications}
+        title="Landing Pages"
+        subtitle="My work on landing pages."
+        type="Landing Page"
+    />
+    <PublicationsSection
+        publications={publications}
+        title="Enterprise Resources"
+        subtitle="My work on landing pages."
+        type="Enterprise Resource" 
+    />
+    <PublicationsSection
+        publications={publications}
+        title="Technical Blogs"
+        subtitle="My work on landing pages."
+        type="Technical Blog" 
+    />
+    <PublicationsSection
+        publications={publications}
+        title="Customer Stories"
+        subtitle="My work on landing pages."
+        type="Customer Story"
+    />
+    <PublicationsSection
+        publications={publications}
+        title="Product Features"
+        subtitle="My work on landing pages."
+        type="Product Feature"
+    />
+</main>
---
+++ apps/tempo-immaterial/src/routes/(landing-pages)/studio/vercel/publications-section.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+    import type { ProfessionalPublications } from './+page.server'
+
+    type PublicationTypes = "Landing Page" | "Technical Blogs" | "Customer Story" | "Enterprise Resource" | "Product Feature";
+
+    export let publications: Array<ProfessionalPublications>;
+    export let title: String;
+    export let subtitle: String;
+    export let type: PublicationTypes;
+</script>
+
+<section>
+    <h2>{title}</h2>
+    <p class="italic">{subtitle}</p>
+    <ul>
+        {#each publications as publication}
+            {#if publication.properties.Type.select.name === type}
+                <li>
+                    <a href={publication.properties.Link.url}>
+                        <h3>
+                            {publication.properties.Name.title[0].plain_text}
+                        </h3>
+                    </a>
+                    <p>
+                        {publication.properties.Date.date.start}
+                    </p>
+                    <p>
+                        {publication.properties.Type.select.name}
+                    </p>
+                </li>
+            {/if}
+        {/each}
+    </ul>
+</section>
+
+<style>
+    h3 {
+        font-weight: 400;
+        color: rgb(34, 65, 103);
+
+        &:hover {
+            text-decoration: underline;
+        }
+    }
+</style>
